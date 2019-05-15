fa40d73f34e37577ea154d4b03c0bfbdb7c2bb48
Dmitry Sokolov <dim778@yandex.ru>
2019-05-15 10:38:04 +0300

    Added scriptsFolder parameter to example.

--- examples/main.cpp
+++ examples/main.cpp
@@ -8,7 +8,7 @@ int main()
     /// MARK: should not have '/' as a last path symbol.
     std::string template_img_path = "/Users/dmitrysokolov/Documents/Diplom_dataset/template_dataset";
     std::string img3 = "/Users/dmitrysokolov/Documents/Diplom_dataset/test_group_images/Derduga_Ryabov_Fedorov.jpg";
-    auto resPair = UUUU::findLabeledFaceRect(img3, template_img_path);
+    auto resPair = UUUU::findLabeledFaceRect(img3, template_img_path, "/Users/dmitrysokolov/ModelAPI/modelAPI");
     auto resMap = resPair.first;
     std::cout << "Known people: \n";
     for (std::map< std::string, UUUU::Coords >::iterator i = resMap.begin(); i != resMap.end(); i++)
